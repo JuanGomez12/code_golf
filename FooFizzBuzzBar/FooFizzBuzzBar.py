e33415b2f32d726ded0bbdb9cdd75b5b29c8f3db
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-01-12 16:29:26 -0500

    Improve FooFizzBuzzBar

--- FooFizzBuzzBar/FooFizzBuzzBar.py
+++ FooFizzBuzzBar/FooFizzBuzzBar.py
@@ -1,6 +1,6 @@
 for i in range(1,1001):
-	cs={2:'Foo',3:'Fizz',5:'Buzz',7:'Bar'}
-	w=''.join([cs[c] for c in cs if not i%c])
+	d={2:'Foo',3:'Fizz',5:'Buzz',7:'Bar'}
+	w=''.join([d[c] for c in d if not i%c])
 	if not w:
 		w=i
 	print(w)
