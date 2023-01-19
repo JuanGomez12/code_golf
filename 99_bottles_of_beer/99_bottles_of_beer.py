994e41d0aff20fc7b8a7586e66184640bace03ea
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-01-19 15:47:55 -0500

    Reduce the chars

--- 99_bottles_of_beer/99_bottles_of_beer.py
+++ 99_bottles_of_beer/99_bottles_of_beer.py
@@ -1,18 +1,19 @@
-d='bottles'
+d=' bottles'
 e=' of beer'
 w=' on the wall'
 def b(i):
 	if i>1:
-		return f"{i} {d}"
+		s=f"{i}{d}"
 	elif i==1:
-		return f"{i} bottle"
+		s=f"{i} bottle"
 	else:
-		return f'no more {d}'
+		s=f'no more{d}'
+	return s
 def c(i):
 	if i>0:
 		s=f'Take one down and pass it around, {b(i-1)}{e}{w}.\n'
 	else:
-		s=f'Go to the store and buy some more, 99 {d}{e}{w}.\n'
+		s=f'Go to the store and buy some more, 99{d}{e}{w}.\n'
 	return s
 for i in range(99,-1,-1):
 	print(b(i).capitalize()+f'{e}{w}, {b(i)}{e}.\n{c(i)}')
