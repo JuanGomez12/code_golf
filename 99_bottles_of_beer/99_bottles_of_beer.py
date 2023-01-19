7538aa40aa726042bc28662511f3b249b1472caa
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-01-19 17:10:36 -0500

    Optimize

--- 99_bottles_of_beer/99_bottles_of_beer.py
+++ 99_bottles_of_beer/99_bottles_of_beer.py
@@ -1,15 +1,10 @@
 def b(i):
-	if i>1:
-		s=f'{i} bottles'
-	elif i==1:
-		s=f'{i} bottle'
-	else:
-		s='no more bottles'
+	if i>1:s=f'{i} bottles'
+	elif i==1:s=f'{i} bottle'
+	else:s='no more bottles'
 	return s+' of beer on the wall'
 def c(i):
-	if i>0:
-		s=f'Take one down and pass it around, {b(i-1)}.\n'
-	else:
-		s=f'Go to the store and buy some more, {b(99)}.\n'
+	if i>0:s=f'Take one down and pass it around, {b(i-1)}.\n'
+	else:s=f'Go to the store and buy some more, {b(99)}.\n'
 	return s
 for i in range(99,-1,-1):print(b(i).capitalize()+f', {b(i)[:-12]}.\n{c(i)}')
