2692f16feb43f0967712dc0e8431ba29a6f3396a
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-02-02 17:59:22 -0500

    Improve score

--- HappyNumbers/HappyNumbers.py
+++ HappyNumbers/HappyNumbers.py
@@ -1,11 +1,8 @@
-def happy(nums):
-	happy_set=[]
-	nums = sum([int(num)**2 for num in str(nums)])
-	while nums not in happy_set:
-		happy_set+=[nums]
-		nums = sum([int(num)**2 for num in str(nums)])
-	return nums
-
+def h(n):
+	s=[]
+	while n not in s:
+		s+=[n]
+		n=sum([int(i)**2 for i in str(n)])
+	return n
 for i in range(201):
-	num = happy(i)
-	if num==1:print(i)
+	if h(i)==1:print(i)
