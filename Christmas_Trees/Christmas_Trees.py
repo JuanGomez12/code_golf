856b257310c17b7174da091a4a83e6f3a6346177
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-02-09 16:27:31 -0500

    Improve score

--- Christmas_Trees/Christmas_Trees.py
+++ Christmas_Trees/Christmas_Trees.py
@@ -1,5 +1,6 @@
 r=range
 p=print
-for i in r(3,10):
-	[p(' '*(i-j+1)+'*'*(2*j-1)) for j in r(1,i+1)]
-	p(' '*(i)+'*\n')
+for i in r(4,11):
+	for j in r(1,i):
+		p(' '*(i-j)+'*'*(2*j-1))
+	p(' '*(i-1)+'*\n')
