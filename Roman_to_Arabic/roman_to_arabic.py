532aa805e89018dc5550c457de7b8e6719e18baa
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-03-16 16:40:11 -0400

    Improve score

--- Roman_to_Arabic/roman_to_arabic.py
+++ Roman_to_Arabic/roman_to_arabic.py
@@ -1,15 +1,12 @@
 import sys
 d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
 for a in sys.argv[1:]:
-    if len(a)>1:
-        n=0
-        b=list(a)
-        while b:
-            m=d[b.pop(0)]
-            if b and m<d[b[0]]:
-                n+=d[b.pop(0)]-m
-            else:
-                n+=m
-    else:
-        n=d[a]
+    n=0
+    b=list(a)
+    while b:
+        m=d[b.pop(0)]
+        if b and m<d[b[0]]:
+            n+=d[b.pop(0)]-m
+        else:
+            n+=m
     print(n)
