6533668181b77182bb11d600328733dc00127afa
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-03-23 16:14:13 -0400

    Improve score

--- Roman_to_Arabic/roman_to_arabic.py
+++ Roman_to_Arabic/roman_to_arabic.py
@@ -1,12 +1,10 @@
 import sys
 d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
 for a in sys.argv[1:]:
-    n=0
-    b=list(a)
-    while b:
-        m=d[b.pop(0)]
-        if b and m<d[b[0]]:
-            n+=d[b.pop(0)]-m
-        else:
-            n+=m
+    b=list(a[::-1])
+    n=p=d[b.pop(0)]
+    for v in b:
+        k=d[v]
+        n=n-k if p>k else n+k
+        p=k
     print(n)
