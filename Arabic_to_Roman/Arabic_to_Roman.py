2b92811023b601e37ee47d68775383ced8f20d38
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-03-30 15:42:18 -0400

    First commit

---
+++ Arabic_to_Roman/Arabic_to_Roman.py
@@ -0,0 +1,12 @@
+import sys
+d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+for a in sys.argv[1:]:
+    b=[]
+    while a>0:
+        if a>1000:
+            b.append('M')
+            a-=1000
+        if a>699:
+            num=
+            b.append()
