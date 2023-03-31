ee0abce2f8f0c623a3a15bc501b7ddf17e6bdb8f
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-03-30 22:03:34 -0400

    Finish hole

--- Arabic_to_Roman/Arabic_to_Roman.py
+++ Arabic_to_Roman/Arabic_to_Roman.py
@@ -1,12 +1,11 @@
 import sys
-d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+d={1:'I',4:'IV',5:'V',9:'IX',10:'X',40:'XL', 50:'L',90:'XC',100:'C',400:'CD',500:'D',900:'CM',1000:'M'}
 for a in sys.argv[1:]:
-    b=[]
+    a=int(a)
+    b=''
     while a>0:
-        if a>1000:
-            b.append('M')
-            a-=1000
-        if a>699:
-            num=
-            b.append()
+        for key in sorted(list(d.keys()), reverse=True):
+            while a>=key:
+                b=b+d[key]
+                a-=key
+    print(b)
