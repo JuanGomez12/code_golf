0274da7312b9b079b8a8d5f45af2dc7add0ebff5
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2023-03-30 22:06:27 -0400

    Improve score

--- Arabic_to_Roman/Arabic_to_Roman.py
+++ Arabic_to_Roman/Arabic_to_Roman.py
@@ -1,11 +1,11 @@
 import sys
 d={1:'I',4:'IV',5:'V',9:'IX',10:'X',40:'XL', 50:'L',90:'XC',100:'C',400:'CD',500:'D',900:'CM',1000:'M'}
 for a in sys.argv[1:]:
-    a=int(a)
-    b=''
-    while a>0:
-        for k in sorted(list(d.keys()), reverse=True):
-            while a>=k:
-                b=b+d[k]
-                a-=k
-    print(b)
+ a=int(a)
+ b=''
+ while a>0:
+  for k in sorted(list(d.keys()), reverse=True):
+   while a>=k:
+    b=b+d[k]
+    a-=k
+ print(b)
